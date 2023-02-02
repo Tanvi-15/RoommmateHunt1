d767eeb33e7c28a972d9ffac8b736dd688ef72ac
tanvi-15 <tanvi.deshpande@somaiya.edu>
2023-02-02 23:25:58 +0530

    add: Login register

--- roommatehunt/lib/main.dart
+++ roommatehunt/lib/main.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:roommatehunt/helper/helper_function.dart';
+import 'package:roommatehunt/pages/auth/login_page.dart';
 import 'package:roommatehunt/pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  const MyApp({Key? key}): super(key:key);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}):super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInState();
+  }
+
+  getUserLoggedInState() async{
+    await HelperFunctions.getUserLoggedInStatus().then((value){
+      if(value!=null){
+        _isSignedIn = value;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: _isSignedIn? const HomePage(): const LoginPage(),
     );
   }
 }
-  
-  
+
+// firebase_auth,firebase_core,cloud_firestore,shared_preferences
--- roommatehunt/lib/pages/home_page.dart
+++ roommatehunt/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:roommatehunt/pages/quiz_page.dart';
 
 class HomePage extends StatefulWidget{
   const HomePage({Key? key}) : super(key: key);
@@ -12,184 +13,38 @@ class _HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Injuries"),
-        actions: [
-          IconButton(onPressed: () {  
-            showSearch(context: context, delegate: CustomSearchDelegate(),);
-          }, icon: const Icon(Icons.search))
-        ],),
-
+      
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: GridView.count(crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10,
-          children: [
-            Container(
-              height: 100,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Burns 🔥'),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Dog Bite'),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Road Accident'),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Sprained Leg'),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Lorem'),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Ipsum'),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Lorem'),
-              ),
-            ),Container(
-              height: 100,
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Ipsum'),
-              ),
-            ),
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Text('Hi Email!', 
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.bold,
+              ),),
+              Text("Please take the quiz so we can find a roommate for you whose interests align with yours",style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              
+            ),),
+              
+              // ElevatedButton(onPressed:() { },
+              // child: null,
+              // ),
+              
+    ],
+  ),
+)
 
-          ],
-        ),
+
+
+
       )
-      // body: Container(
-      //   child: Padding(
-      //     padding: const EdgeInsets.all(10.0),
-      //     child: GridView(
-      //       children:[
-      //         Container(decoration: BoxDecoration(
-      //           borderRadius: BorderRadius.circular(20),
-      //           color: Colors.red,),
-      //           ),
-                
-      //         Container(decoration: BoxDecoration(
-      //           borderRadius: BorderRadius.circular(20),
-      //           color: Colors.yellow,),
-      //           ),
-      //         Container(decoration: BoxDecoration(
-      //           borderRadius: BorderRadius.circular(20),
-      //           color: Colors.green,),
-      //           ),
-      //         Container(decoration: BoxDecoration(
-      //           borderRadius: BorderRadius.circular(20),
-      //           color: Colors.blue,),
-      //           ),
-      //       ],
-      //       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      //         crossAxisCount: 2,
-      //         mainAxisSpacing: 10,
-      //         crossAxisSpacing: 10 ),
-      //     ),
-      //   ),
-      // ),
-      
-
     );
-  }
-}
-
-class CustomSearchDelegate extends SearchDelegate{
-  List<String> searchTerms =[
-    'Apple',
-    'Banana',
-    'Pear',
-    'Watermelons',
-    'Oranges',
-    'Blueberries',
-    'Strawberries',
-    'Raspberries',
-  ];
-  @override
-  List<Widget> buildActions(BuildContext context){
-    return[
-      IconButton(
-        onPressed: (){
-
-        }, 
-      icon: const Icon(Icons.clear),),
-    ];
-  }
-
-  @override 
-  Widget buildLeading(BuildContext context){
-    return IconButton(onPressed:(){
-      close(context, null);
-      }, 
-      icon: Icon(Icons.arrow_back),);
-  }
-
-  @override 
-  Widget buildResults(BuildContext context){
-    List<String> matchQuery=[];
-    for (var fruit in searchTerms){
-      if(fruit.toLowerCase().contains(query.toLowerCase())){
-        matchQuery.add(fruit);
-      }
-    }
-
-    return ListView.builder(itemCount: matchQuery.length,
-    itemBuilder: (context, index) {
-      var result = matchQuery[index];
-      return ListTile(
-        title: Text(result),
-      );
-    });
-  }
-
-  @override 
-  Widget buildSuggestions(BuildContext context){
-    List<String> matchQuery=[];
-    for (var fruit in searchTerms){
-      if(fruit.toLowerCase().contains(query.toLowerCase())){
-        matchQuery.add(fruit);
-      }
-    }
-
-    return ListView.builder(itemCount: matchQuery.length,
-    itemBuilder: (context, index) {
-      var result = matchQuery[index];
-      return ListTile(
-        title: Text(result),
-      );
-    });
   }
 }
