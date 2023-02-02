37b77abaea445ec20bb5fae8cd3126330db34a6c
unknown <vedashreejoshi20@gmail.com>
2023-02-03 00:40:06 +0530

    quiz

--- roommatehunt/lib/pages/quiz_page.dart
+++ roommatehunt/lib/pages/quiz_page.dart
@@ -255,5 +255,290 @@
 //      ),
 //     );
 //   }
-// }    
+// }
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // Hide the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Tell Us Something About Yourself',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // The inital group value
+  String _selectedGender = 'male';
+  String _selectedCity = 'Mumbai';
+  String _selectedStatus = 'College';
+  String _selectedNo = '1';
+  String _selecteda = 'aesthetic';
+  String _selecteds = 'silent';
+  final budgetController = TextEditingController();
+  final areaController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Tell Us Something About Yourself',
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Please let us know your gender:'),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'male',
+                    groupValue: _selectedGender,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedGender = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Male'),
+                ),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'female',
+                    groupValue: _selectedGender,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedGender = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Female'),
+                ),
+                const SizedBox(height: 25),
+                const Text('Please let us know your city:'),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'Mumbai',
+                    groupValue: _selectedCity,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedCity = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Mumbai'),
+                ),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'Delhi',
+                    groupValue: _selectedCity,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedCity = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Delhi'),
+                ),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'Jaipur',
+                    groupValue: _selectedCity,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedCity = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Jaipur'),
+                ),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'Bangalore',
+                    groupValue: _selectedCity,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedCity = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Bangalore'),
+                ),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'Kolkata',
+                    groupValue: _selectedCity,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedCity = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Kolkata'),
+                ),
+                const SizedBox(height: 25),
+                const Text('Please let us know your Locality:'),
+                TextFormField(
+                  controller: areaController,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration:InputDecoration(
+                    labelText:'',
+                    hintText:'Your Locality',
+                  ),
+                ),
+                const SizedBox(height: 25),
+                const Text('Please let us know your Work Status:'),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'College',
+                    groupValue: _selectedStatus,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedStatus = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('College'),
+                ),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'Work',
+                    groupValue: _selectedStatus,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedStatus = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Work'),
+                ),
+                const SizedBox(height: 25),
+                const Text('Please let us know your nature:'),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'Silent Reader',
+                    groupValue: _selecteds,
+                    onChanged: (value) {
+                      setState(() {
+                        _selecteds = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Silent Reader'),
+                ),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'Party Person',
+                    groupValue: _selecteds,
+                    onChanged: (value) {
+                      setState(() {
+                        _selecteds = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Party Person'),
+                ),
+                const SizedBox(height: 25),
+                const Text('Please let us know your design preferences:'),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'Aesthetic',
+                    groupValue: _selecteda,
+                    onChanged: (value) {
+                      setState(() {
+                        _selecteda = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Aesthetic'),
+                ),
+                ListTile(
+                  leading: Radio<String>(
+                    value: 'Minimalistic',
+                    groupValue: _selecteda,
+                    onChanged: (value) {
+                      setState(() {
+                        _selecteds = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('Minimilistic'),
+                ),
+                const SizedBox(height: 25),
+                const Text('Please let us know your Budget:'),
+                TextFormField(
+                  controller: budgetController,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration:InputDecoration(
+                    labelText:'',
+                    hintText:'Your Budget',
+                  ),
+                ),
+                const SizedBox(height: 25),
+                const Text('Please let us know your number of preferred roomates:'),
+                ListTile(
+                  leading: Radio<String>(
+                    value: '1',
+                    groupValue: _selectedNo,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedNo = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('1'),
+                ),
+                ListTile(
+                  leading: Radio<String>(
+                    value: '2',
+                    groupValue: _selectedNo,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedNo = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('2'),
+                ),
+                ListTile(
+                  leading: Radio<String>(
+                    value: '3',
+                    groupValue: _selectedNo,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedNo = value!;
+                      });
+                    },
+                  ),
+                  title: const Text('3'),
+                ),
+                const SizedBox(height: 25),
+              ],
+            )
+        ),
+      ),
+    );
+  }
+}
       
