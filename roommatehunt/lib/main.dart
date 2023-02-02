9ebf10a22b85e3edf16af31105edc334555b4528
tanvi-15 <tanvi.deshpande@somaiya.edu>
2023-02-02 18:08:15 +0530

    home

--- roommatehunt/lib/main.dart
+++ roommatehunt/lib/main.dart
@@ -2,68 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:roommatehunt/pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future main() async{
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}): super(key:key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Main page'),
+      home: HomePage(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello! Welcome to First Aid',
-              ),
-            ElevatedButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context)=> const HomePage()));
-            }, child: const Text('Go to First Aid'),
-          )
-          ],
-          
-        )
-        // 
-
-      ),
-      
-    );
-  }
-}
+  
+  
